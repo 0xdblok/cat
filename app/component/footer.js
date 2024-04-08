c0182f7bbf92f7d71969d6837bd553fac87545c8
dblok0x <dblok0x@gmail.com>
2024-04-08 18:22:15 +0200

    ee

--- app/component/footer.js
+++ app/component/footer.js
@@ -2,18 +2,32 @@ import React from "react";
 import Image from "next/image";
 import x from "../asset/x.png";
 import telegram from "../asset/telegram.png";
+import tele from "../asset/tele.svg";
+import xx from "../asset/xx.svg";
 import "animate.css";
 export default function Footer() {
   return (
     <div className="animate__animated animate__fadeInUp">
-      <div className="flex  pt-10 justify-end pb-4 pr-6">
+      <div className="flex gap-2 pt-10 justify-end pb-4 pr-6">
         <a>
           {" "}
-          <Image src={telegram} alt="Picture of a cat" width={28} height={28} />
+          <Image
+            src={tele}
+            className="lg:w-[40px] lg:h-[40px]"
+            alt="Picture of a cat"
+            width={30}
+            height={30}
+          />
         </a>
         <a>
           {" "}
-          <Image src={x} alt="Picture of a cat" width={30} height={30} />
+          <Image
+            className="lg:w-[40px] lg:h-[40px]"
+            src={xx}
+            alt="Picture of a cat "
+            width={30}
+            height={30}
+          />
         </a>
       </div>
     </div>
