0ea474eb33b5bec088e8ddfd0df02dde7affeb70
dblok0x <dblok0x@gmail.com>
2024-04-08 17:22:05 +0200

    ee

--- app/component/hero.js
+++ app/component/hero.js
@@ -7,7 +7,7 @@ export default function Hero() {
       <p className="[text-shadow:2px_2px_0px_rgba(46,56,125)]   font-customfont text-white  text-center text-2xl pt-14">
         DUMB CAT
       </p>
-      <div className="px-36 ">
+      <div className="px-[70px] ">
         <Image src={hero} alt="Picture of a cat" width={450} height={450} />
       </div>
       <p className="text-center  text-2xl text-[#2E387D] pt-4">
