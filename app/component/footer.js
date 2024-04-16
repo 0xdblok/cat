e2beff099f9b8721e3cd201a9df861378b952deb
0xdblok <dblok0x@gmail.com>
2024-04-16 14:40:46 +0200

    e

--- app/component/footer.js
+++ app/component/footer.js
@@ -9,16 +9,6 @@ export default function Footer() {
   return (
     <div className="animate__animated animate__fadeInUp">
       <div className="flex gap-2 pt-10 justify-end pb-4 pr-6">
-        <a href="">
-          {" "}
-          <Image
-            src={tele}
-            className="lg:w-[40px] lg:h-[40px]"
-            alt="Picture of a cat"
-            width={30}
-            height={30}
-          />
-        </a>
         <a href="https://x.com/dumbcatonbase">
           {" "}
           <Image
