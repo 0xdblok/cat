3553b3e22bc235c3dc781ab75998e80741595b25
0xdblok <dblok0x@gmail.com>
2024-04-16 14:40:04 +0200

    ee

--- app/component/footer.js
+++ app/component/footer.js
@@ -9,9 +9,7 @@ export default function Footer() {
   return (
     <div className="animate__animated animate__fadeInUp">
       <div className="flex gap-2 pt-10 justify-end pb-4 pr-6">
-        <a
-          href="
-https://t.me/+zxtCG8a4gJI5YzBl">
+        <a href="">
           {" "}
           <Image
             src={tele}
